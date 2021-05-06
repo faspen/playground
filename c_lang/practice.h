86382381d697774bd90949ba44fb4beb31f4e834
faspen <2335@holbertonschool.com>
2021-05-06 20:11:00 +0000

    folders

---
+++ c_lang/practice.h
@@ -0,0 +1,15 @@
+#ifndef TRAIN_H
+#define TRAIN_H
+
+/* Libraries */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+#endif
